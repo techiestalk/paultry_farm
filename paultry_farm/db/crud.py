61015302eef6ca4b63345d573ee80ae84c53fc6d
swadhi <cswadhikar@gmail.com>
2019-03-04 09:27:52 +0530

    API requirement

--- paultry_farm/db/crud.py
+++ paultry_farm/db/crud.py
@@ -4,5 +4,13 @@
 
 
 class Database:
-    pass
+    """
+        Use sqlite3 to create below APIs to connect and interact with
+        the database
 
+        1. def connect()            - Connect to the database
+        2. def execute(query)       - Execute the given query. Commit if write operation.
+        3. def execute_get(query)   - Execute the given query and return output
+        4. def close()              - Close database connection
+    """
+    pass
--- paultry_farm/models/feed_medicine.py
+++ paultry_farm/models/feed_medicine.py
@@ -4,7 +4,17 @@
 
 
 class Feed:
-    pass
+    """
+        FEED table schema:  FEED_ID, FEED_NAME
+
+        Create class methods for the below methods:
+
+        1. def add_feed(cls, feed_name) - Should add the given feed to the Feed Table
+        2. def update_feed(cls, feed_name, new_feed_name) - Rename the existing feed name to new name
+        3. def delete_feed(cls, feed_name) - Delete given feed from table
+        4. def get_all_feed(cls, feed_name) - Query the database and return the feed names as list
+
+    """
 
 
 class Medicine:
